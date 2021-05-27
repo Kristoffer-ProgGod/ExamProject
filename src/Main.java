b203c7faf8f058493f0c1091c89237e01c4841d9
maipil99 <mai-pil@hotmail.com>
2021-05-27 10:00:41 +0200

    startpage + ID's

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("User Interface/Homepage.fxml"));
         primaryStage.setTitle("Timeline");
-        primaryStage.setScene(new Scene(root, 1700, 900));
+        primaryStage.setScene(new Scene(root, 1700, 955));
         primaryStage.show();
 
 
