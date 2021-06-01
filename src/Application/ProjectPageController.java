0a5bae9a7b55c996524b166cd26a709bb44dc10d
maipil99 <mai-pil@hotmail.com>
2021-06-01 16:20:08 +0200

    UI details, notes on timeline, drag/drop

--- src/Application/ProjectPageController.java
+++ src/Application/ProjectPageController.java
@@ -9,6 +9,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -57,7 +58,14 @@ public class ProjectPageController implements Initializable {
     @FXML
     TextArea noteArea;
     @FXML
+    TextArea noteTextArea;
+    @FXML
+    Pane notePane;
+    @FXML
     TextField referenceField;
+
+    @FXML
+    Label projectNameLabel;
 
     @FXML
     Pane timeline;
@@ -66,6 +74,11 @@ public class ProjectPageController implements Initializable {
     ListView<Notebank> notebankList;
     @FXML
     ListView<Note> noteListView;
+
+    @FXML
+    ScrollBar scrollBarHorizontal;
+    @FXML
+    ScrollBar scrollBarVertical;
 
     //Create a new note
     public void createNote(ActionEvent event) {
@@ -136,90 +149,100 @@ public class ProjectPageController implements Initializable {
 
 
             editNoteButton.setText("Edit");
-            editNoteButton.setPrefSize(70, 30);
-            editNoteButton.setLayoutX(230);
-            editNoteButton.setLayoutY(140);
+            editNoteButton.setPrefSize(50, 15);
+            editNoteButton.setLayoutX(60);
+            editNoteButton.setLayoutY(110);
             editNoteButton.setOnAction(editTextArea);
-            editNoteButton.setStyle("-fx-font-weight: bolder");
+            editNoteButton.setStyle("-fx-font-size: 13");
 
             deleteNoteButton.setText("Delete");
-            deleteNoteButton.setPrefSize(70, 30);
-            deleteNoteButton.setLayoutX(230);
-            deleteNoteButton.setLayoutY(170);
-            deleteNoteButton.setStyle("-fx-font-weight: bolder");
+            deleteNoteButton.setPrefSize(60, 15);
+            deleteNoteButton.setLayoutX(115);
+            deleteNoteButton.setLayoutY(110);
+            deleteNoteButton.setStyle("-fx-font-size: 13");
 //            deleteNoteButton.setOnAction(removeNote);
 
 
             noteTextArea.setStyle("-fx-background-color: white");
-            noteTextArea.setPrefSize(300, 200);
+            noteTextArea.setPrefSize(180, 135);
             noteTextArea.setEffect(dropShadow);
+            noteTextArea.setLayoutY(10);
 
-            notePane.setPrefSize(300, 230);
+            notePane.setPrefSize(90, 140);
+            notePane.setStyle("-fx-background-color: darkviolet");
+            notePane.setOnMouseEntered(dragNote);
+            noteTextArea.setEditable(false);
+            noteTextArea.setOnMouseEntered(dragNote);
 
             notePane.setLayoutX(posX);
-            posX = posX + 320;
+            posX = posX + 10;
             notePane.getChildren().addAll(noteTextArea, editNoteButton, deleteNoteButton);
 
             timeline.getChildren().add(notePane);
             noteTextArea.setText(chosenNote.getText());
-            noteTextArea.setPrefSize(300, 200);
-            noteTextArea.setEditable(false);
-            noteTextArea.setEffect(dropShadow);
+
 
             SingletonMediator.getInstance().getCurrentProject().getTimeline().add(chosenNote);
         }
     }
 
-    public void saveProject(ActionEvent event){
+    public void saveProject(ActionEvent event) {
 
     }
 
-    public void initTimeline(Project project){
+    public void loadProjectName(){
+       projectNameLabel.setText("Project Name: " + SingletonMediator.getInstance().getCurrentProject().getProjectTitle());
+
+    }
+
+    public void initTimeline(Project project) {
+        loadProjectName();
         {
-            for (Note note: project.getTimeline()) {
+            for (Note note : project.getTimeline()) {
 
-            editNoteButton = new Button();
-            deleteNoteButton = new Button();
-            Pane notePane = new Pane();
-            TextArea noteTextArea = new TextArea();
+                editNoteButton = new Button();
+                deleteNoteButton = new Button();
+                Pane notePane = new Pane();
+                TextArea noteTextArea = new TextArea();
 
-            DropShadow dropShadow = new DropShadow();
-            dropShadow.setRadius(5.0);
-            dropShadow.setOffsetX(3.0);
-            dropShadow.setOffsetY(3.0);
-            dropShadow.setColor(Color.color(0, 0, 0));
+                DropShadow dropShadow = new DropShadow();
+                dropShadow.setRadius(5.0);
+                dropShadow.setOffsetX(3.0);
+                dropShadow.setOffsetY(3.0);
+                dropShadow.setColor(Color.color(0, 0, 0));
 
 
-            editNoteButton.setText("Edit");
-            editNoteButton.setPrefSize(70, 30);
-            editNoteButton.setLayoutX(230);
-            editNoteButton.setLayoutY(140);
-            editNoteButton.setOnAction(editTextArea);
-            editNoteButton.setStyle("-fx-font-weight: bolder");
+                editNoteButton.setText("Edit");
+                editNoteButton.setPrefSize(50, 15);
+                editNoteButton.setLayoutX(60);
+                editNoteButton.setLayoutY(110);
+                editNoteButton.setOnAction(editTextArea);
+                editNoteButton.setStyle("-fx-font-size: 13");
 
-            deleteNoteButton.setText("Delete");
-            deleteNoteButton.setPrefSize(70, 30);
-            deleteNoteButton.setLayoutX(230);
-            deleteNoteButton.setLayoutY(170);
-            deleteNoteButton.setStyle("-fx-font-weight: bolder");
+                deleteNoteButton.setText("Delete");
+                deleteNoteButton.setPrefSize(60, 15);
+                deleteNoteButton.setLayoutX(115);
+                deleteNoteButton.setLayoutY(110);
+                deleteNoteButton.setStyle("-fx-font-size: 13");
 //            deleteNoteButton.setOnAction(removeNote);
 
 
-            noteTextArea.setStyle("-fx-background-color: white");
-            noteTextArea.setPrefSize(300, 200);
-            noteTextArea.setEffect(dropShadow);
+                noteTextArea.setStyle("-fx-background-color: white");
+                noteTextArea.setPrefSize(180, 135);
+                noteTextArea.setEffect(dropShadow);
+                noteTextArea.setLayoutY(10);
 
-            notePane.setPrefSize(300, 230);
+                notePane.setPrefSize(90, 140);
+                notePane.setStyle("-fx-background-color: darkviolet");
+                notePane.setOnMouseEntered(dragNote);
+                noteTextArea.setEditable(false);
+                noteTextArea.setOnMouseEntered(dragNote);
 
-            notePane.setLayoutX(posX);
-            posX = posX + 320;
-            notePane.getChildren().addAll(noteTextArea, editNoteButton, deleteNoteButton);
+                notePane.setLayoutX(posX);
+                posX = posX + 10;
+                notePane.getChildren().addAll(noteTextArea, editNoteButton, deleteNoteButton);
 
-            timeline.getChildren().add(notePane);
-            noteTextArea.setText(note.getText());
-            noteTextArea.setPrefSize(300, 200);
-            noteTextArea.setEditable(false);
-            noteTextArea.setEffect(dropShadow);
+                timeline.getChildren().add(notePane);
             }
         }
     }
@@ -237,7 +260,7 @@ public class ProjectPageController implements Initializable {
         }
     };
 
-//    //wtf it does not work. Why not?!?!?!?
+    //    //wtf it does not work. Why not?!?!?!?
 //    //HJÆÆÆÆÆÆÆÆÆLP!!
 //    // Does not know which Note object is selected only which notePane,
 //    // does not remove 320 from posX if more than one is removed at a time.
@@ -263,39 +286,59 @@ public class ProjectPageController implements Initializable {
 //
 //    }
 //
-//
-//    public void dragNote(MouseDragEvent event) {
-//        Label note = (Label) event.getSource();
-//
-//        timeline.setOnDragDetected(new EventHandler<MouseEvent>() {
-//            @Override
-//            public void handle(MouseEvent event) {
-//                Dragboard db = note.startDragAndDrop(TransferMode.MOVE);
-//                event.consume();
-//            }
-//        });
-//
-//
-//        timeline.setOnDragOver(new EventHandler<DragEvent>() {
-//            @Override
-//            public void handle(DragEvent event) {
-//                if (event.getSource() != timeline) {
-//                    event.acceptTransferModes(TransferMode.MOVE);
-//                }
-//                event.consume();
-//            }
-//        });
-//
-//        timeline.setOnDragDropped(new EventHandler<DragEvent>() {
-//            @Override
-//            public void handle(DragEvent event) {
-//                event.setDropCompleted(true);
-//                event.consume();
-//            }
-//        });
-//
-//    }
+    final Delta dragDelta = new Delta();
+    EventHandler<MouseEvent> dragNote = event -> {
+        Pane notePane = (Pane) event.getSource();
 
+
+        notePane.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                dragDelta.x = notePane.getLayoutX() - event.getSceneX();
+                dragDelta.y = notePane.getLayoutY() - event.getSceneY();
+                notePane.setCursor(Cursor.MOVE);
+            }
+        });
+        notePane.setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                notePane.setCursor(Cursor.HAND);
+                event.consume();
+            }
+        });
+        notePane.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                notePane.setLayoutX(mouseEvent.getSceneX() + dragDelta.x);
+                notePane.setLayoutY(mouseEvent.getSceneY() + dragDelta.y);
+            }
+        });
+        notePane.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                notePane.setCursor(Cursor.HAND);
+            }
+        });
+
+        timeline.setOnDragOver(new EventHandler<DragEvent>() {
+            @Override
+            public void handle(DragEvent event) {
+                System.out.println("drag over");
+                if (event.getSource() == timeline) {
+                    event.acceptTransferModes(TransferMode.MOVE);
+                }else event.acceptTransferModes(TransferMode.NONE);
+                event.consume();
+            }
+        });
+
+        notePane.setOnDragDetected(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                /* drag was detected, start drag-and-drop gesture*/
+                System.out.println("onDragDetected: ");
+                event.consume();
+            }
+        });
+    };
 }
-
-
+        class Delta {
+            double x, y;}
